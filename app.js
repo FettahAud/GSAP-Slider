8e9059d8fa3781dac9df3480b9ebb4f495dd13e0
FettahAud <abooo2003adoany@gmail.com>
2021-11-24 23:42:06 +0300

    display the contne on load

--- app.js
+++ app.js
@@ -7,7 +7,6 @@ const slide2 = document.querySelector('.slide2');
 const slide3 = document.querySelector('.slide3');
 const tl = gsap.timeline({ paused: true });
 
-
 window.addEventListener('load', () => {
     tl
         .to(slide1, { clipPath: 'polygon(7% 7%, 93% 7%, 93% 93%, 7% 93%)' })
@@ -26,9 +25,3 @@ window.addEventListener('load', () => {
         tl.play()
     });
 })
-
-
-
-// cont.addEventListener('mouseover', () => tl.play())
-slideRight.addEventListener('click', () => tl.play())
-slideLeft.addEventListener('click', () => tl.reverse())
