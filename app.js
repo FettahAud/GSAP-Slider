d44d60e2488fb8fe791334974cccc8eaaa1ea73d
FettahAud <abooo2003adoany@gmail.com>
2021-11-24 23:56:43 +0300

    Added better ease

--- app.js
+++ app.js
@@ -5,9 +5,10 @@ const svg = document.querySelector('.arrows svg');
 const slide1 = document.querySelector('.slide1');
 const slide2 = document.querySelector('.slide2');
 const slide3 = document.querySelector('.slide3');
-const tl = gsap.timeline({ paused: true });
+const tl = gsap.timeline({ paused: true, defaults: { ease: Power3.easeInOut } });
 
 window.addEventListener('load', () => {
+    cont.style.visibility = 'visible';
     tl
         .to(slide1, { clipPath: 'polygon(7% 7%, 93% 7%, 93% 93%, 7% 93%)' })
         .to(slide1, { clipPath: 'polygon(93% 7%, 93% 7%, 93% 93%, 93% 93%)' }, '+=1')
